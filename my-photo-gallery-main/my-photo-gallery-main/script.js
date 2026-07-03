b465ed855aa8b8c8064dcc6153362556c1fa468a
Minh Anh Lê Ngọc <minhanhlengoc27@gmail.com>
2026-07-03 12:27:16 +0700

    Add files via upload

---
+++ my-photo-gallery-main/my-photo-gallery-main/script.js
@@ -0,0 +1,17 @@
+function upDate(previewPic) {
+    console.log("mouse over image");
+    console.log(previewPic.alt);
+    console.log(previewPic.src);
+
+    document.getElementById("image").innerHTML = previewPic.alt;
+
+    document.getElementById("image").style.backgroundImage =
+        "url('" + previewPic.src + "')";
+}
+
+function unDo() {
+    document.getElementById("image").style.backgroundImage = "url('')";
+
+    document.getElementById("image").innerHTML =
+        "Hover over an image below to display here.";
+}
